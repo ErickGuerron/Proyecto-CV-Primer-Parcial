2053a9012980cfda99d8f4d4edb4130b2d42ac29
Mabe Zapata <patylo1970maria@gmail.com>
2025-10-30 16:46:01 -0500

    Se agrego comentario en la clase Punina

--- src/interfaces/Punina.java
+++ src/interfaces/Punina.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package interfaces;
 
-/**
- *
- * @author Anthony
- */
+
 public class Punina {
-    
+    //Prueba de escritura en la clase Punina por Maria Zapata
 }
